eed69413234501aef2369c2f8ff38325cbe3e58c
Harshit Tomar <139328522+harshit-tomar-0@users.noreply.github.com>
2025-11-23 13:02:09 -0500

    Add files via upload

---
+++ password_fuzzer.py
@@ -0,0 +1,24 @@
+#python3
+#random password fuzzer
+from random import choice
+example_words=["harsh","sharma","01031956","mumbai","iitbombay"]
+import string
+symbol=choice(list(string.punctuation))
+example_limit=8
+temp=None
+new_example=[]
+for i in example_words:
+    temp=int(example_limit/2)
+    if len(i)>temp+1:
+        new_example.extend(i.partition(i[temp:temp+1]))
+    else:
+        ...
+print(new_example)
+for i in range(0,len(new_example)):
+    symbol=choice(list(string.punctuation))
+    for j in range(0,len(new_example)):
+        temp=new_example[i]+symbol+new_example[j]
+        if len(temp)<=8:
+            print("::",temp)
+        else:
+            print("%",temp)
